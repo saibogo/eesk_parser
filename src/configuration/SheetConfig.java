c910ad8121d8d2d10178af08b3113c738c1b9616
Andrey Gleykh <gleykh@gmail.com>
2021-02-13 11:40:24 +0500

    Refactoring

--- src/configuration/SheetConfig.java
+++ src/configuration/SheetConfig.java
@@ -5,5 +5,16 @@ public class SheetConfig {
     public static final int SheetIndex = 0;
     public static final int startCellIndex = 0;
     public static final int stopCellIndex = 9;
+    public static final String column1 = "Регион";
+    public static final String column2 = "Район";
+    public static final String column3 = "Населенный пункт";
+    public static final String column4 = "Улица";
+    public static final String column5 = "Дата отключения";
+    public static final String column6 = "Время отключения";
+    public static final String column7 = "Дата включения";
+    public static final String column8 = "Время включения";
+    public static final String column9 = "Филиал";
+    public static final String column10 = "РЭС";
+
 
 }
